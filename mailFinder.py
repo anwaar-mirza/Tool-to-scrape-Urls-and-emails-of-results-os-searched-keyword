2f5e6188d9c60d4dc38ac61d13dd3d199aefdf40
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-02 16:37:12 +0500

    Update mailFinder.py

--- mailFinder.py
+++ mailFinder.py
@@ -1,10 +1,10 @@
-import undetected_chromedriver as uc
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.edge.service import Service
 from bs4 import BeautifulSoup
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 import selenium.webdriver.support.expected_conditions as EC
 import time
 import pickle
@@ -13,13 +13,13 @@ import re
 class EmailFinder:
     def __init__(self):
         options = Options()
-        service = Service(ChromeDriverManager().install())
+        service = Service(EdgeChromiumDriverManager().install())
         options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-infobars")
         options.add_argument("--disable-blink-features=AutomationControlled")
-        self.driver = uc.Chrome(service=service, options=options, version_main=120)
+        self.driver = webdriver.Edge(service=service, options=options, version_main=120)
         self.driver.get("https://www.facebook.com")
         with open("face_cook.pkl", "rb") as file: 
                 cookies = pickle.load(file)
