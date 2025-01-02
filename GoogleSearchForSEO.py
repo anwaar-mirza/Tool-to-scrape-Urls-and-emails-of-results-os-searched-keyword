74a6ea4bb233f0900baf14986b123b6d33bffae5
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-02 16:50:48 +0500

    Update GoogleSearchForSEO.py

--- GoogleSearchForSEO.py
+++ GoogleSearchForSEO.py
@@ -1,4 +1,4 @@
-icfrom selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver. common.action_chains import ActionChains
