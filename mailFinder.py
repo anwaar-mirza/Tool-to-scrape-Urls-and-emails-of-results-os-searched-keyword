b77bed4a1a0215f809913b9d0dc7e33ac8d4347e
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-02 16:46:01 +0500

    Update mailFinder.py

--- mailFinder.py
+++ mailFinder.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+vifrom selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.edge.options import Options
@@ -19,7 +19,7 @@ class EmailFinder:
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-infobars")
         options.add_argument("--disable-blink-features=AutomationControlled")
-        self.driver = webdriver.Edge(options=options)
+        self.driver = webdriver.Edge(service=service, options=options)
         self.driver.get("https://www.facebook.com")
         with open("face_cook.pkl", "rb") as file: 
                 cookies = pickle.load(file)
