74d12a86aca7b84e6ad85053890749682a1cf592
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-06 11:58:26 +0500

    Update GoogleSearchForSEO.py

--- GoogleSearchForSEO.py
+++ GoogleSearchForSEO.py
@@ -16,7 +16,7 @@ import os
 
 class SEOFinding:
     def __init__(self):
-        chrome_service = Service(ChromeDriverManager().install())
+        # chrome_service = Service(ChromeDriverManager().install())
         options = uc.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("--disable-gpu")
@@ -26,7 +26,7 @@ class SEOFinding:
         fake_agent = UserAgent()
         options.add_argument(f"user-agent={fake_agent.chrome}")
         options.add_argument("--disable-blink-features=AutomationControlled")
-        self.driver = uc.Chrome(service=chrome_service, options=options)
+        self.driver = uc.Chrome(options=options)
         self.driver.maximize_window()
         self.wait = WebDriverWait(self.driver, 3)
         self.action = ActionChains(self.driver)
