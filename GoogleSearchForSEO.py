c8c0a4d20ce12c24983f61dd84ce5f80bc3dcf24
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-06 12:43:06 +0500

    Update GoogleSearchForSEO.py

--- GoogleSearchForSEO.py
+++ GoogleSearchForSEO.py
@@ -20,7 +20,7 @@ class SEOFinding:
         service = Service(executable_path=ChromeDriverManager().install())
         options = Options()
         if not options.binary_location:
-            options.binary_location = ChromeDriverManager().install()
+            options.binary_location = "C:/Program Files/Google/Chrome/Application/chrome.exe"
         options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
