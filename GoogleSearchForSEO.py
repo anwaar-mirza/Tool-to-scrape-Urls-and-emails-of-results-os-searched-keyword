ed0a7387d68bda04c93051eff26676e3805f2969
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-06 12:15:44 +0500

    Update GoogleSearchForSEO.py

--- GoogleSearchForSEO.py
+++ GoogleSearchForSEO.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver. common.action_chains import ActionChains
@@ -27,7 +27,7 @@ class SEOFinding:
         fake_agent = UserAgent()
         options.add_argument(f"user-agent={fake_agent.chrome}")
         options.add_argument("--disable-blink-features=AutomationControlled")
-        self.driver = webdriver.Chrome(service=service, options=options)
+        self.driver = uc.Chrome(service=service, options=options)
         self.driver.maximize_window()
         self.wait = WebDriverWait(self.driver, 3)
         self.action = ActionChains(self.driver)
