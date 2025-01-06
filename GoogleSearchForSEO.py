f366f7ece643e82c543655991cb3a4ac9287f151
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-06 12:02:07 +0500

    Update GoogleSearchForSEO.py

--- GoogleSearchForSEO.py
+++ GoogleSearchForSEO.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver. common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
