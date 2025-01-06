72a4cc16a663d0b39b51773e6a1e75f9b66927d9
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-06 15:54:39 +0500

    Update mailFinder.py

--- mailFinder.py
+++ mailFinder.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 import selenium.webdriver.support.expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
