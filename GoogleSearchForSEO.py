109becb0c7a11f28fd126e0e27362ffc440e9454
Anwaar Mirza <mirzaanwaar52@gmail.com>
2025-01-06 16:10:16 +0500

    Update GoogleSearchForSEO.py

--- GoogleSearchForSEO.py
+++ GoogleSearchForSEO.py
@@ -1,4 +1,5 @@
 import undetected_chromedriver as uc
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver. common.action_chains import ActionChains
